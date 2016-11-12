12ac05642df853259ae750c0ceb5366e35996d3b
Christophe Chapuis <chris.chapuis@gmail.com>
2016-11-12 20:20:31 +0100

    Exit app if qml file can't be loaded
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
     engine.addImportPath("./imports");
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+           return 1;
+
 
     return app.exec();
 }
